80a4fd5f27bdcb49d2283951441af33af1412a7c
SUNIL GANESH K <139485838+codeswithganesh@users.noreply.github.com>
2024-05-15 15:01:23 +0530

    Update Application.cs

--- CarrerHubApplication/Application.cs
+++ CarrerHubApplication/Application.cs
@@ -1,10 +1,4 @@
-﻿using CareerHub.Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using CareerHub.Service;
 
 namespace CareerHub.CarrerHubApplication
 {
@@ -19,7 +13,8 @@ namespace CareerHub.CarrerHubApplication
             {
 
                 bool flag1 = true;
-                Console.WriteLine("----------------------------Welcome to the CarrerHub Menu-----------------------");
+                Console.WriteLine("----------------------------Welcome to the CarrerHub Menu-----------------------:)");
+                Console.WriteLine("CarrerHub is a digital platform or system that facilitates the process of job searching and recruitment");
                 Console.WriteLine("1.JobListingInterface\n2.CompanyInterface\n3.ApplicantInterface\n4.DataBaseManagerInterface\n5.DatabaseTasks\n6.Exit");
                 Console.WriteLine("Choose any One Option");
                 int input = int.Parse(Console.ReadLine());
@@ -111,27 +106,39 @@ namespace CareerHub.CarrerHubApplication
                     case 4:
                         while (flag1)
                         {
-                            Console.WriteLine("Welcome to the OrderDetail Interface");
-                            Console.WriteLine("1.CalculateSubtotal\n2.GetOrderDetailInfo\n3.UpdateQuantity\n4.AddDiscount\n5.back to MainMenu");
+                            Console.WriteLine("Welcome to the DataBase Manager Interface");
+                            Console.WriteLine("1.InsertJobListing\n2.InsertCompany\n3.InsertApplicant\n4.InsertJobApplication\n5.GetJobListing\n6.GetCompanies\n7.GetApplicants\n8.GetApplicationsForJobId\n9.BackToMainMenu");
                             Console.WriteLine("Choose any One Option");
                             int input1 = int.Parse(Console.ReadLine());
                             switch (input1)
                             {
                                 case 1:
-                                    ods.CalculateSubtotal();
+                                    ds.InsertJobListing();
                                     break;
 
                                 case 2:
-                                    ods.GetOrderDetailInfo();
+                                    ds.InsertCompany();
                                     break;
                                 case 3:
-                                    ods.UpdateQuantity();
+                                    ds.InsertApplicant();
                                     break;
                                 case 4:
-                                    Console.WriteLine("Not done yet");
+                                    ds.InsertJobApplication();
+                                    break;
+                                case 5:
+                                    ds.GetJobListings();
+                                    break;
+                                case 6:
+                                    ds.GetCompanies();
+                                    break;
+                                case 7:
+                                    ds.GetApplicants();
+                                    break;
+                                case 8:
+                                    ds.GetApplicationsForJob();
                                     break;
 
-                                case 5:
+                                case 9:
                                     flag1 = false;
                                     break;
                                 default:
@@ -143,46 +150,32 @@ namespace CareerHub.CarrerHubApplication
                     case 5:
                         while (flag1)
                         {
-                            Console.WriteLine("Welcome to the Inventory Interface");
-                            Console.WriteLine(@"1.GetProduct\n2.GetQuantityInStock\n3.AddToInventory\n4.RemoveFromInventory
-                                \n5.UpdateStockQuantity\n6.IsProductAvailable\n7.GetInventoryValue\n8.ListLowStockProducts\n9.ListOutOfStockProducts\n10.ListAllProducts\n11.back to MainMenu");
+                            Console.WriteLine("Welcome to the DataBase Tasks");
+                            Console.WriteLine(@"1.JobListing Retreval\n2.Applicant Profile Creation \n3.Job Application Submission \n4.Company Job Posting
+                                \n5.Salary Range Query\n6.Back to Main Menu");
                             Console.WriteLine("Choose any One Option");
                             int input1 = int.Parse(Console.ReadLine());
                             switch (input1)
                             {
                                 case 1:
-                                    ins.GetProduct();
+                                    ds.GetJobListing();
                                     break;
 
                                 case 2:
-                                    ins.GetQuantityInStock();
+                                    ds.InsertApplicant();
                                     break;
                                 case 3:
-                                    ins.AddToInventory();
+                                    ds.InsertJobApplication();
                                     break;
                                 case 4:
-                                    ins.RemoveFromInventory();
+                                    ms.PostJob();
                                     break;
 
                                 case 5:
-                                    ins.UpdateStockQuantity();
+                                    ds.GetJobListingSalary();
                                     break;
+                                
                                 case 6:
-                                    ins.IsProductAvailable();
-                                    break;
-                                case 7:
-                                    ins.GetInventoryValue();
-                                    break;
-                                case 8:
-                                    ins.ListLowStockProducts();
-                                    break;
-                                case 9:
-                                    ins.ListOutOfStockProducts();
-                                    break;
-                                case 10:
-                                    ins.ListAllProducts();
-                                    break;
-                                case 11:
                                     flag1 = false;
                                     break;
                                 default:
@@ -194,6 +187,7 @@ namespace CareerHub.CarrerHubApplication
 
 
                     case 6:
+                        Console.WriteLine("!!!!:) Thank You Visit Agian :}!!!!");
                         flag = false;
                         break;
                     default:
